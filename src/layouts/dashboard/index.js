97a94093956e19dbea04174ae94659594d2c209f
lyricalboii <lyricalboii001@gmail.com>
2023-04-01 16:01:30 +0530

    serverv1

---
+++ src/layouts/dashboard/index.js
@@ -0,0 +1,138 @@
+import React, { useEffect } from "react";
+import { Stack } from "@mui/material";
+import { Navigate, Outlet } from "react-router-dom";
+import useResponsive from "../../hooks/useResponsive";
+import SideNav from "./SideNav";
+import { useDispatch, useSelector } from "react-redux";
+import { SelectConversation, showSnackbar } from "../../redux/slices/app";
+import { socket, connectSocket } from "../../socket";
+import {
+  UpdateDirectConversation,
+  AddDirectConversation,
+  AddDirectMessage,
+} from "../../redux/slices/conversation";
+import CallNotification from "../../sections/Dashboard/CallNotification";
+import { PushToAudioCallQueue } from "../../redux/slices/audioCall";
+
+const DashboardLayout = () => {
+  const isDesktop = useResponsive("up", "md");
+  const { open_notification_dialog } = useSelector((state) => state.audioCall);
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  const { conversations, current_conversation } = useSelector(
+    (state) => state.conversation.direct_chat
+  );
+  const dispatch = useDispatch();
+
+  const user_id = window.localStorage.getItem("user_id");
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      window.onload = function () {
+        if (!window.location.hash) {
+          window.location = window.location + "#loaded";
+          window.location.reload();
+        }
+      };
+
+      window.onload();
+
+      if (!socket) {
+        connectSocket(user_id);
+      }
+
+      socket.on("audio_call_notification", (data) => {
+        // TODO => dispatch an action to add this in call_queue
+
+        dispatch(PushToAudioCallQueue(data));
+      });
+
+      socket.on("new_message", (data) => {
+        const message = data.message;
+        console.log(current_conversation, data);
+        // check if msg we got is from currently selected conversation
+        if (current_conversation.id === data.conversation_id) {
+          dispatch(
+            AddDirectMessage({
+              id: message._id,
+              type: "msg",
+              subtype: message.type,
+              message: message.text,
+              incoming: message.to === user_id,
+              outgoing: message.from === user_id,
+            })
+          );
+        }
+      });
+
+      socket.on("start_chat", (data) => {
+        console.log(data);
+        // add / update to conversation list
+        const existing_conversation = conversations.find(
+          (el) => el.id === data._id
+        );
+        if (existing_conversation) {
+          // update direct conversation
+          dispatch(UpdateDirectConversation({ conversation: data }));
+        } else {
+          // add direct conversation
+          dispatch(AddDirectConversation({ conversation: data }));
+        }
+        dispatch(SelectConversation({ room_id: data._id }));
+      });
+
+      socket.on("new_friend_request", (data) => {
+        dispatch(
+          showSnackbar({
+            severity: "success",
+            message: "New friend request received",
+          })
+        );
+      });
+
+      socket.on("request_accepted", (data) => {
+        dispatch(
+          showSnackbar({
+            severity: "success",
+            message: "Friend Request Accepted",
+          })
+        );
+      });
+
+      socket.on("request_sent", (data) => {
+        dispatch(showSnackbar({ severity: "success", message: data.message }));
+      });
+    }
+
+    // Remove event listener on component unmount
+    return () => {
+      socket?.off("new_friend_request");
+      socket?.off("request_accepted");
+      socket?.off("request_sent");
+      socket?.off("start_chat");
+      socket?.off("new_message");
+      socket?.off("audio_call_notification");
+    };
+  }, [isLoggedIn, socket]);
+
+  if (!isLoggedIn) {
+    return <Navigate to={"/auth/login"} />;
+  }
+
+  return (
+    <>
+      <Stack direction="row">
+        {isDesktop && (
+          // SideBar
+          <SideNav />
+        )}
+
+        <Outlet />
+      </Stack>
+      {open_notification_dialog && (
+        <CallNotification open={open_notification_dialog} />
+      )}
+    </>
+  );
+};
+
+export default DashboardLayout;
